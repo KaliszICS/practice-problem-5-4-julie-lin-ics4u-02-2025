ae3aa979155bc0b8595687bca81cc41f7f2dacf7
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-05-22 00:51:19 +0000

    Complete 5.4 Practice Problem

---
+++ src/main/java/MusicPlayer.java
@@ -0,0 +1,66 @@
+class MusicPlayer implements Player {
+    
+    private boolean onOff;
+    private String[] musicList;
+    private String currentSong;
+    private int volume;
+    private int index = 0;
+
+    public MusicPlayer(String[] list) {
+        this.musicList = list;
+        this.volume = 0;
+        this.onOff = false;
+        this.currentSong = "";
+    }
+
+    @Override
+    public void start() {
+        if(!this.onOff){
+            this.onOff = true;
+            this.currentSong = musicList[0];
+            index = 0;
+        }
+    }
+
+    @Override
+    public void stop() {
+        if(this.onOff){
+            this.onOff = false;
+            this.currentSong = "";
+        }
+    }
+
+    public boolean getOnOff() {
+        return this.onOff;
+    }
+
+     @Override
+    public void volumeUp(){
+        this.volume += 1;
+    }
+
+    @Override 
+    public void volumeDown(){
+        this.volume -= 1;
+    }
+
+    @Override
+    public int getVolume(){
+        return this.volume;
+    }
+
+    public void next(){
+        index++;
+        this.currentSong = musicList[index];
+    }
+
+    public void previous(){
+        index--;
+        this.currentSong = musicList[index];
+    }
+
+    public String getCurrentSong(){
+        return this.currentSong;
+    }
+
+}
---
+++ src/main/java/RadioPlayer.java
@@ -0,0 +1,67 @@
+public class RadioPlayer implements Player {
+
+    private boolean onOff;
+    private double[] stationList;
+    private int volume;
+    private double station;
+    private int index = 0;
+
+    public RadioPlayer(double[] list) {
+        this.stationList = list;
+        this.volume = 0;
+        this.onOff = false;
+        this.station = 0.0;
+    }
+
+    
+    @Override
+    public void start() {
+        if(!this.onOff){
+            this.onOff = true;
+            this.station = stationList[0];
+            index = 0;
+        }
+    }
+
+    @Override
+    public void stop() {
+        if(this.onOff){
+            this.onOff = false;
+            this.station = 0;
+        }
+    }
+
+    public boolean getOnOff() {
+        return this.onOff;
+    }
+
+     @Override
+    public void volumeUp(){
+        this.volume += 2;
+    }
+
+    @Override 
+    public void volumeDown(){
+        this.volume -= 2;
+    }
+
+    @Override
+    public int getVolume(){
+        return this.volume;
+    }
+
+    public void next(){
+        index++;
+        this.station = stationList[index];
+    }
+
+    public void previous(){
+        index--;
+        this.station = stationList[index];
+    }
+
+    public double getStation(){
+        return this.station;
+    }
+
+}
